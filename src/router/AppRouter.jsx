a2ea4ea6bffe00c73034c0463676558bd1045fc6
Malejo <alejoramirez6@yahoo.com>
2023-05-09 15:00:41 -0500

    Fin de aplicación Heroes App

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 import { LoginPage } from "../auth";
 import { HeroesRoutes } from "../heroes/Routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 
 export const AppRouter = () => {
@@ -10,8 +12,23 @@ export const AppRouter = () => {
 
         <Routes>
      
-            <Route path="login" element={<LoginPage />} />
-            <Route path="/*" element={<HeroesRoutes />} />
+            <Route path="login" element = {
+              <PublicRoute>
+                <Routes>
+                  <Route path="/*" element={<LoginPage />} />
+                </Routes>
+              </PublicRoute>
+            }/>
+            
+            {/* <Route path="login" element={<LoginPage />} /> */}
+
+            <Route path="/*" element= {
+              <PrivateRoute>
+                <HeroesRoutes />
+              </PrivateRoute>
+            }/>
+
+           {/* <Route path="/*" element={<HeroesRoutes />} /> */}
 
         </Routes>
    
